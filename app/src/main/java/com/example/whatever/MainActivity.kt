99f6f0118ef1f94a57cb68535a364983c9e7b968
Ivan Briseno <ibriseno@mail.sfsu.edu>
2019-02-23 18:21:48 -0800

    Splash Theme now works, The first screen has a fade in, small icon, fade out while increasing in size.

--- app/src/main/java/com/example/whatever/MainActivity.kt
+++ app/src/main/java/com/example/whatever/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.example.whatever
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        overridePendingTransition(R.anim.fade_in,R.anim.fade_out)
+        id_logo.startAnimation(AnimationUtils.loadAnimation(this,R.anim.fade_in))
+
+        Handler().postDelayed({
+            id_logo.startAnimation(AnimationUtils.loadAnimation(this,R.anim.splash_out))
+            Handler().postDelayed({
+                id_logo.visibility = View.GONE
+                startActivity(Intent(this,SplashActivity::class.java))
+                finish()
+            },500)
+        },2500)
+
     }
 }
+
--- app/src/main/java/com/example/whatever/SplashActivity.kt
+++ app/src/main/java/com/example/whatever/SplashActivity.kt
@@ -8,5 +8,6 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        overridePendingTransition(R.anim.fade_in,R.anim.fade_out)
     }
 }
